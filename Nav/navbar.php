fc4724836c19d09b9ce53a58d065eb2eba1ca6fd
Muhammad Azka <azkaganteng50@gmail.com>
2023-09-24 14:09:41 +0700

    test

--- Nav/navbar.php
+++ Nav/navbar.php
@@ -29,7 +29,7 @@ session_start();
                 </div>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="../Page/loginPage.php">Login</a>
+                <a class="nav-link" href="../Page/loginPage.php">Account</a>
             </li>
             <?php
             if(isset($_SESSION['Type']) && $_SESSION['Type'] == 1) { ?>
