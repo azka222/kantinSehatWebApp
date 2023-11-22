2a1315c88eee120a4a548f28ec8b318845810341
arvynrf <arvyn.bang@gmail.com>
2023-11-22 23:06:00 +0700

    updated register

--- Auth/register.php
+++ Auth/register.php
@@ -1,46 +1,52 @@
 <?php
-    include_once "./connection.php";
+include_once "./connection.php";
 
-    if($_SERVER['REQUEST_METHOD'] == "POST"){
-        $name = $_POST['name'];
-        $address = $_POST['address'];
-        $password = $_POST['password'];
-        $san_pas = htmlspecialchars($password);
-        $con_pas = $_POST['con_pas'];
-        $len = strlen($password);
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $name = $_POST['name'];
+    $address = $_POST['address'];
+    $password = $_POST['password'];
+    $con_pas = $_POST['con_pas'];
 
-        if(!empty($name) && !empty($address) && !empty($password)){
-            if($con_pas != $password){
-                header('location:../Page/registerPage.php?error=Password not match!');
-                exit();
-            }
-            // else if($len < 8){
-            //     header('location:../Page/registerPage.php?error=Password is too short!');
-            //     exit();
-            // }
-            else{
-                $hashed = password_hash($san_pas, PASSWORD_DEFAULT);
-                $query = "INSERT INTO nonAdmin (name, address, password) VALUES('$name', '$address', '$hashed')";
-                mysqli_query($conn, $query);
-                header('location:../Page/registerPage.php');
-                header('location:../Page/registerPage.php?success=Success!');
-                
-            }
-   
-        }
-        else if(empty($name)){
-            header('location:../Page/registerPage.php?error=Name is required!');
-            exit();
-        }
-        else if(empty($address)){
-            header('location:../Page/registerPage.php?error=Address is required!');
-            exit();
-        }
-        else if(empty($password)){
-            header('location:../Page/registerPage.php?error=Password is required!');
-            exit(); 
-        }
+    // Validate input
+    if (empty($name) || empty($address) || empty($password)) {
+        header('location:../Page/registerPage.php?error=All fields are required!');
+        exit();
     }
-    
-    
+
+    // Validate password match
+    if ($con_pas != $password) {
+        header('location:../Page/registerPage.php?error=Passwords do not match!');
+        exit();
+    }
+
+    // Validate password length
+    $len = strlen($password);
+    if ($len < 8) {
+        header('location:../Page/registerPage.php?error=Password is too short!');
+        exit();
+    }
+
+    // Hash the password securely
+    $san_pas = htmlspecialchars($password);
+    $hashed = password_hash($san_pas, PASSWORD_DEFAULT);
+
+    // Use prepared statement to insert data
+    $query = "INSERT INTO nonAdmin (name, address, password) VALUES(?, ?, ?)";
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "sss", $name, $address, $hashed);
+
+    if (mysqli_stmt_execute($stmt)) {
+        // Redirect on success
+        header('location:../Page/registerPage.php?success=Registration successful!');
+        exit();
+    } else {
+        // Handle the case where the statement execution fails
+        header('location:../Page/registerPage.php?error=Registration failed. Please try again.');
+        exit();
+    }
+
+    // Close the prepared statement and database connection
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+}
 ?>
