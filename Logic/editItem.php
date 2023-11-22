be6af85aba6094b3ca5fe80ef7cdd838dbf9d5c2
arvynrf <arvyn.bang@gmail.com>
2023-11-22 23:09:06 +0700

    added edit feature

--- Logic/editItem.php
+++ Logic/editItem.php
@@ -3,6 +3,8 @@
 include_once "../Auth/connection.php";
 session_start();
 
+$_SESSION['Type'] = 1;
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (empty($_POST['editName'])) {
         $editName = htmlspecialchars($_POST['idProductName']);
@@ -17,32 +19,29 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $editStock = htmlspecialchars($_POST['editStock']);
     $id = htmlspecialchars($_POST['idProduct']);  
     
-    // Menggunakan mysqli_real_escape_string untuk mengamankan variabel yang digunakan dalam kueri SQL
     $editName = mysqli_real_escape_string($conn, $editName);
     $editCaption = mysqli_real_escape_string($conn, $editCaption);
     $editStock = mysqli_real_escape_string($conn, $editStock);
     $id = mysqli_real_escape_string($conn, $id);
-    
-    if (!empty($editStock) && is_numeric($editStock)) {
-        // Menggunakan prepared statement
-        $query = "UPDATE menu SET Name = ?, Caption = ?, qtt = ? WHERE id = ?";
-        $stmt = mysqli_prepare($conn, $query);
-        mysqli_stmt_bind_param($stmt, "ssis", $editName, $editCaption, $editStock, $id);
-        if(mysqli_stmt_execute($stmt)){
-            if(mysqli_stmt_affected_rows($stmt) == 1){
-                header('location:../Modal/modal.php');
-                exit();
-            }
-        }
 
-        // Tutup prepared statement
-        mysqli_stmt_close($stmt);
+    $query = "UPDATE menu SET Name = ?, Caption = ?, qtt = ? WHERE id = ?";
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "ssis", $editName, $editCaption, $editStock, $id);
+
+    if (mysqli_stmt_execute($stmt)) {
+        if (mysqli_stmt_affected_rows($stmt) == 1) {
+            mysqli_stmt_close($stmt);
+            mysqli_close($conn);
+            header('location:../Page/menuPage.php');
+            exit();
+        } else {
+            header('location:../Page/menuPage.php');
+        }
     } else {
-        echo "Masukkan data yang akan diperbarui.";
-        exit();
+        echo "Error executing statement: " . mysqli_stmt_error($stmt);
     }
-   
-    // Tutup koneksi
+
+    mysqli_stmt_close($stmt);
     mysqli_close($conn);
 }
 ?>
